f073ac0a8cc6b11505e127a388678becf6602720
hajekad3 <hajekad3@fit.cvut.cz>
2022-10-25 04:07:14 +0200

    init source

---
+++ hajekad3/src/main/java/cz/cvut/fit/hajekad3/SemestralApplication.java
@@ -0,0 +1,13 @@
+package cz.cvut.fit.hajekad3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemestralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemestralApplication.class, args);
+	}
+
+}
