256c32e97fe8937207cd327fbc0f8f749a171a9f
hajekad3 <hajekad3@fit.cvut.cz>
2023-01-05 00:12:19 +0100

    docs dont work

--- reservantor/src/main/java/cz/cvut/fit/hajekad3/reservantor/ReservantorApplication.java
+++ reservantor/src/main/java/cz/cvut/fit/hajekad3/reservantor/ReservantorApplication.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @Import(CorsConfig.class)
+@OpenAPIDefinition
 public class ReservantorApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ReservantorApplication.class, args);
